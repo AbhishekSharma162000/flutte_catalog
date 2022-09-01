959b98bc67199ac172fe9ebb06c2f8628c7b028d
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2022-09-01 21:49:53 +0530

    DAY 2 ----> Add AppBar

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final int days = 50;
+  final String name = "abhishek";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Text("Welcome to $days days by $name"),
+      ),
+    );
+  }
+}
