75ded134ceb1c16961aa4599b2f798338cac89ef
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2022-09-06 12:50:59 +0530

    ListViewBuilder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  final int days = 50;
-  final String name = "abhishek";
-
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Catalog App",
+        appBar: AppBar(
+          title: const Text(
+            "Catalog App",
+          ),
         ),
-      ),
-      drawer: const MyDrawer(),
-      body: Center(
-        child: Text("Welcome to $days days by $name"),
-      ),
-    );
+        drawer: const MyDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            },
+          ),
+        ));
   }
 }
