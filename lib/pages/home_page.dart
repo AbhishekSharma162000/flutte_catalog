73f1a7ed9df54b0d411de06f3b75f08a95fc261e
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2022-09-07 10:36:37 +0530

    Grid View, Grid TIle

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,11 +43,41 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      crossAxisCount: 2),
                   itemCount: CatalogModel.items.length,
-                  itemBuilder: (context, index) => ItemWidget(
-                        item: CatalogModel.items[index],
-                      ))
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items[index];
+                    return Card(
+                        clipBehavior: Clip.antiAlias,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: GridTile(
+                            header: Container(
+                              padding: const EdgeInsets.all(12),
+                              child: Text(
+                                item.name,
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              decoration: const BoxDecoration(
+                                color: Colors.indigo,
+                              ),
+                            ),
+                            footer: Container(
+                              padding: const EdgeInsets.all(12),
+                              child: Text(
+                                item.price.toString(),
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              decoration: const BoxDecoration(
+                                color: Colors.black,
+                              ),
+                            ),
+                            child: Image.network(item.image)));
+                  })
               : const Center(
                   child: CircularProgressIndicator(),
                 ),
